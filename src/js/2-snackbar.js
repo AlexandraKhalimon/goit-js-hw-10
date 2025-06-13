d1b09ef2a53a0522c0d7d772e8b966011f11b687
Alexandra Khalimon <lexikhalimon@gmail.com>
2025-06-13 23:11:11 +0300

    Add snackbar files

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,3 +1,48 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
+const promiseForm = document.querySelector(".form");
+const delayInput = document.querySelector(`input[name="delay"]`);
+const fulfilledBtn = document.querySelector(`input[value="fulfilled"]`);
+const rejectedBtn = document.querySelector(`input[value="rejected"]`);
+
+promiseForm.addEventListener("submit", handleForm);
+
+function handleForm(event) {
+    event.preventDefault();
+    const delay = delayInput.value;
+    const fulfilled = fulfilledBtn.checked;
+    const rejected = rejectedBtn.checked;
+    promiseForm.reset();
+    
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (fulfilled) {
+                resolve(`✅ Fulfilled promise in ${delay}ms`);
+            };
+            if (rejected) {
+                reject(`❌ Rejected promise in ${delay}ms`)
+            }
+        }, delay);
+    });
+
+    promise
+        .then(
+            value => {
+                iziToast.show({
+                    message: value,
+                    messageSize: `18`,
+                    color: `green`,
+                    position: `topCenter`,
+                })
+            })
+        .catch(
+            error => {
+                iziToast.show({
+                    message: error,
+                    messageSize: `18`,
+                    color: `red`,
+                    position: `topCenter`,
+                })
+            });
+}
